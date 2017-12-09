7258224190ed72e53033c0dec93301eeadf514df
vivet <vivet@vivetonline.com>
2017-12-09 22:44:38 +0100

    Added test project

---
+++ DynamicExpression.Test/CriteriaBuilderTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DynamicExpression.Test
+{
+    [TestClass]
+    public class CriteriaBuilderTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
