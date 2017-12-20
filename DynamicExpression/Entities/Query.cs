828bfea6cb6ceb48436677a6ea482bcc198a91ea
vivet <vivet@vivetonline.com>
2017-12-20 09:09:33 +0100

    Added default initialization of Query Sorting and Paging property.

--- DynamicExpression/Entities/Query.cs
+++ DynamicExpression/Entities/Query.cs
@@ -6,17 +6,17 @@ namespace DynamicExpression.Entities
     public class Query : IQuery
     {
         /// <inheritdoc />
-        public virtual IOrdering Order { get; set; }
+        public virtual IOrdering Order { get; set; } = new Ordering();
 
         /// <inheritdoc />
-        public virtual IPagination Paging { get; set; }
+        public virtual IPagination Paging { get; set; } = new Pagination();
     }
 
     /// <inheritdoc cref="IQuery{TCriteria}"/>
     public class Query<TCriteria> : Query, IQuery<TCriteria>
-        where TCriteria : IQueryCriteria
+        where TCriteria : IQueryCriteria, new()
     {
         /// <inheritdoc />
-        public virtual TCriteria Criteria { get; set; }
+        public virtual TCriteria Criteria { get; set; } = new TCriteria();
     }
 }
