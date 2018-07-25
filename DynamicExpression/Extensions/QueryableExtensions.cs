977d8bf4b04eeeb36871916e32879c197d8e52d7
Michael Vivet <vivet@vivetonline.com>
2018-07-26 00:03:09 +0200

    Fixed issue with Ordering by value types

--- DynamicExpression/Extensions/QueryableExtensions.cs
+++ DynamicExpression/Extensions/QueryableExtensions.cs
@@ -105,22 +105,54 @@ namespace DynamicExpression.Extensions
                 var expression = Expression.Lambda<Func<T, Guid>>(property, parameter);
                 return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
             }
-
-            if (property.Type == typeof(TimeSpan))
+            else if (property.Type == typeof(TimeSpan))
             {
                 var expression = Expression.Lambda<Func<T, TimeSpan>>(property, parameter);
                 return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
             }
-
-            if (property.Type == typeof(DateTime))
+            else if (property.Type == typeof(DateTime))
             {
                 var expression = Expression.Lambda<Func<T, DateTime>>(property, parameter);
                 return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
             }
-
-            if (property.Type == typeof(DateTimeOffset))
+            else if (property.Type == typeof(DateTimeOffset))
             {
                 var expression = Expression.Lambda<Func<T, DateTimeOffset>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            if (property.Type == typeof(byte))
+            {
+                var expression = Expression.Lambda<Func<T, byte>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(int))
+            {
+                var expression = Expression.Lambda<Func<T, int>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(long))
+            {
+                var expression = Expression.Lambda<Func<T, long>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(float))
+            {
+                var expression = Expression.Lambda<Func<T, float>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(double))
+            {
+                var expression = Expression.Lambda<Func<T, double>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(decimal))
+            {
+                var expression = Expression.Lambda<Func<T, decimal>>(property, parameter);
+                return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
+            }
+            else if (property.Type == typeof(bool))
+            {
+                var expression = Expression.Lambda<Func<T, bool>>(property, parameter);
                 return ordering.Direction == OrderingDirection.Asc ? source.OrderBy(expression) : source.OrderByDescending(expression);
             }
             else
