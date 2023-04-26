6e46509664193a8bd2ee3e5bb70f8a9e7ae8f054
Michael Vivet <miv@parseport.com>
2023-04-26 06:37:21 +0200

    Added Range limitation to Pagination.Count (1, 1000)

--- DynamicExpression/Entities/Pagination.cs
+++ DynamicExpression/Entities/Pagination.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace DynamicExpression.Entities;
 
@@ -11,11 +12,13 @@ public class Pagination
     /// Number.
     /// </summary>
     [DefaultValue(1)]
+    [Range(1, int.MaxValue)]
     public virtual int Number { get; set; } = 1;
 
     /// <summary>
     /// Count.
     /// </summary>
     [DefaultValue(25)]
+    [Range(1, 1000)]
     public virtual int Count { get; set; } = 25;
 }
